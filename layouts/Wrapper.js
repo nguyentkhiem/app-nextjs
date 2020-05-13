9cde02e611fdc0928b38fd6cbf6df753d7fa3e8a
nguyentkhiem <nguyenthanhkhiem81196@gmail.com>
2020-05-13 21:53:21 +0700

    init

---
+++ layouts/Wrapper.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+const Wrapper = styled.footer`
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+
+  main {
+    flex: 1;
+    width: 1200px;
+    margin: auto;
+  }
+`
+
+export default Wrapper
